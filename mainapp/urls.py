c82b28b8bb7d1bd25f2a88b6b164aabb93024e99
Sarah Brahimi <71637499+sarahbrah7@users.noreply.github.com>
2022-11-05 20:00:19 +0000

    Created base

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from mainapp.views import index 
+from mainapp.views import index, streamers_api
 
 urlpatterns = [
     path('', index),
+    path('api/streamers', streamers_api),
 ]
