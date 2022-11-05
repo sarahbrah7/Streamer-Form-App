623380b7ce723510b3d608ffb7e524aeb30b3b79
Sarah Brahimi <71637499+sarahbrah7@users.noreply.github.com>
2022-11-05 20:36:50 +0000

    views.py updated

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, JsonResponse
 
 from mainapp.models import Streamer
 
 def index(request: HttpRequest) -> HttpResponse:
-    n = Streamer.objects.all().count()
-    # return HttpResponse(f"Hello world, at the moment there are {n} streamers")
     title = "My cool food app"
     return render(request, "mainapp/index.html", {
         'title': title,
-        'n': f(n)
+        'n': Streamer.objects.all().count()
     })
 
-def f(n: int) -> int:
-    return n + 1
+def streamers_api(request):
+    if request.method == 'GET':
+        return JsonResponse({
+            'streamers': [
+                streamer.to_dict() for streamer in Streamer.objects.all()
+            ]
+        })
