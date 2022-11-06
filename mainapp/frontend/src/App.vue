b4e11f6891df287c5a22983e05c6445ca308a39c
Sarah Brahimi <71637499+sarahbrah7@users.noreply.github.com>
2022-11-06 13:31:17 +0000

    basic vue made (also installed bootstrap)

---
+++ mainapp/frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    HELLO WORLD {{ name }}
+    <div>{{ counter }}</div>
+    <button @click="counter++">Increment</button>
+  </div>
+</template>
+
+<script>
+export default{
+  data() {
+    return {
+      name: "TITLE",
+      counter: 0,
+    }
+  }
+}
+</script>
