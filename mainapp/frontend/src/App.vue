3715b1f64630cb1497714ce1880b12a8982bae90
Sarah Brahimi <71637499+sarahbrah7@users.noreply.github.com>
2022-11-06 13:51:25 +0000

    added cors headers and basic vue info

--- mainapp/frontend/src/App.vue
+++ mainapp/frontend/src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    HELLO WORLD {{ name }}
-    <div>{{ counter }}</div>
-    <button @click="counter++">Increment</button>
+    <button @click="fetchStreamers">Fetch Streamers</button>
+    <div>
+      <ul>
+        <li v-for="streamer in streamers">
+          {{ streamer.name}}
+          <span v-if="streamer.twitch">(Twitch Streamer)</span>
+          <span v-if="!streamer.twitch">(Youtube Streamer)</span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -10,8 +17,15 @@
 export default{
   data() {
     return {
-      name: "TITLE",
-      counter: 0,
+      streamers: [],
+    }
+  },
+  methods : {
+    async fetchStreamers() {
+      //perform an ajax request to fetch the list of streamers
+      let response = await fetch("http://localhost:8000/api/streamers")
+      let data = await response.json()
+      this.streamers = data.streamers
     }
   }
 }
